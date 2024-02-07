e776375bd610919a4913e343b441bb9f01c8bb3a
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2024-02-07 10:29:23 +0200

    Implemented dot and inference

--- main.go
+++ main.go
@@ -25,11 +25,52 @@ func TestSigmoid(t *testing.T) {
 	}
 }
 
+func dot(a []float64, b []float64) (res float64) {
+	for i := range a {
+		res += a[i] * b[i]
+	}
+	return res
+}
+
+func TestDot(t *testing.T) {
+	type Input struct {
+		a, b []float64
+	}
+	for num, tc := range []struct {
+		input Input
+		want  float64
+	}{
+		{Input{a: []float64{1, 2}, b: []float64{2, 1}}, 4},
+		{Input{a: []float64{0, 0}, b: []float64{0, 0}}, 0},
+		{Input{a: []float64{1, 1}, b: []float64{1, 1}}, 2},
+		{Input{a: []float64{-1, -1}, b: []float64{1, 1}}, -2},
+		{Input{a: []float64{3, 4}, b: []float64{5, 6}}, 39},
+		{Input{a: []float64{1.5, 2.5}, b: []float64{2.5, 1.5}}, 7.5},
+		{Input{a: []float64{0.5, 0.5}, b: []float64{0.5, 0.5}}, 0.5},
+	} {
+		if got := dot(tc.input.a, tc.input.b); got != tc.want {
+			t.Errorf("Failed test No.%v: got = %v, want = %v", num, got, tc.want)
+		}
+	}
+}
+
+func prediction(x []float64, w []float64, b float64) float64 {
+	return sigmoid(dot(w, x) + b)
+}
+
+func inference(inputs [][]float64, w []float64, b float64) (probabilities []float64) {
+	for _, x := range inputs {
+		probabilities = append(probabilities, prediction(x, w, b))
+	}
+	return probabilities
+}
+
 func main() {
 	testing.Main(
 		func(a, b string) (bool, error) { return a == b, nil },
 		[]testing.InternalTest{
-			{Name: "Test SecondLargest", F: TestSigmoid},
+			{Name: "Test Sigmoid", F: TestSigmoid},
+			{Name: "Test Dot", F: TestDot},
 		}, nil, nil,
 	)
 }
