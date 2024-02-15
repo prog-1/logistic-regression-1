81fab7a60b50435f482d625e533b8555b9b1a7a3
vay4ie <vay4ie@gmail.com>
2024-02-15 15:02:39 +0200

    Added point separation with color

--- plot.go
+++ plot.go
@@ -32,20 +32,34 @@ func (a *App) updatePlot(w []float64, b float64, inputs [][]float64, y []float64
 
 	p := plot.New() //initializing plot
 
-	red := color.RGBA{150, 0, 0, 255}
-	//green := color.RGBA{0, 0, 150, 255}
-
 	//#################### Points ##############################
 
-	var points plotter.XYs //initializing point plotter
+	//Colors
+	blue := color.RGBA{0, 0, 255, 255}
+	red := color.RGBA{255, 0, 0, 255}
 
+	//Plotters
+	var greenPlotter plotter.XYs //initializing green point plotter
+	var redPlotter plotter.XYs   //initializing red point plotter
+
+	//Distributing the points to separate plotters
 	for i := 0; i < numberOfPoints; i++ { //for every point
-		points = append(points, plotter.XY{X: inputs[i][0], Y: inputs[i][1]}) //Saving all points in plotter
+		if y[i] == 0 { //if the current point is false/0/negative
+			greenPlotter = append(greenPlotter, plotter.XY{X: inputs[i][0], Y: inputs[i][1]}) //Saving the point in green plotter
+		} else { //if the current point is true/1/positive
+			redPlotter = append(redPlotter, plotter.XY{X: inputs[i][0], Y: inputs[i][1]}) //Saving the point in red plotter
+		}
 	}
-	scatter, _ := plotter.NewScatter(points) //creating new scatter from point data
-	scatter.Color = red
 
-	p.Add(scatter)
+	//Green scatter
+	greenScatter, _ := plotter.NewScatter(greenPlotter) //creating new scatter from point data
+	greenScatter.Color = blue
+	p.Add(greenScatter)
+
+	//Red scatter
+	redScatter, _ := plotter.NewScatter(redPlotter) //creating new scatter from point data
+	redScatter.Color = red
+	p.Add(redScatter)
 
 	//##################### Ebiten #############################
 
